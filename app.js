63b56cb62ac1d58fd78ffffdded9ca91752dbe43
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-07-20 00:29:08 +0300

    add more pages

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 let tasks = [];
+let workList = [];
 
 app.use(bodyParser.urlencoded({extended:true})); // to use the body parser
 app.use(express.static('public'))
@@ -14,17 +15,32 @@ app.get('/', (req, res)=>{
 
     let currentDay = new Date().toLocaleDateString('en-us', {weekday:"long", day: 'numeric', month:'long'}); // long day to display day name
     
-    res.render('list', {kindOfDay: currentDay, newListItems: tasks});
+    res.render('list', {ListTitle: currentDay, newListItems: tasks});
 });
 
+
+app.get('/work', (req,res) =>{
+    res.render('list', {ListTitle: 'work List', newListItems: workList});
+});
 //to get items from the html
 
 app.post('/', (req, res)=>{
     let task = req.body.newItem;
-    tasks.push(task);
-    res.redirect('/');
+    if (req.body.list === 'work'){
+        workList.push(task);
+        res.redirect('/work')
+    } else{
+        tasks.push(task);
+        res.redirect('/');
+    }
 });
 
+
+app.post('/work', (req, res)=>{
+    let item = req.body.newItem;
+    workList.push(item);
+    res.redirect('/work')
+})
 
 
 app.listen(3000, ()=>{
